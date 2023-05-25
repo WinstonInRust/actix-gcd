b0bd03d82bfde48b1857913fc09b477ff12ea596
WinstonInRust <rustprogrammeren@gmail.com>
2023-05-25 13:46:24 +0200

    Added a post handler function

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
 use actix_web::{web, App, HttpResponse, HttpServer};
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct GcdParameters {
+    n: u64,
+    m: u64,
+}
+
 fn main() {
-    let server = HttpServer::new(|| App::new().route("/", web::get().to(get_index)));
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(get_index))
+            .route("/gcd", web::post().to(post_gcd))
+    });
 
     println!("Serving on http://localhost:2000...");
     server
@@ -21,4 +33,22 @@ fn get_index() -> HttpResponse {
                 </form>
             "#,
     )
+}
+
+fn post_gcd(form: web::Form<GcdParameters>) -> HttpResponse {
+    if form.n == 0 && form.m == 0 {
+        return HttpResponse::BadRequest()
+            .content_type("text/html")
+            .body("Computng the GCD with zero is boring.");
+    }
+
+    let response = format!(
+        "The Greatest Common Divisor of the numbers {} and {} \
+                            is <b>{}</b>\n",
+        form.n,
+        form.m,
+        gcd(form.n, form.m)
+    );
+
+    HttpResponse::Ok().content_type("text/html").body(response)
 }
